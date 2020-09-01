a2559c2f04d21696dc14d563d206c8d02a043611
Владимир <vladimirincog@users.noreply.github.com>
2020-09-02 02:06:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,36 @@
 let navItem = document.getElementsByClassName('item-nav');              //Элементы меню
 let navOpenButton = document.getElementsByClassName('navOpenButton');   //Стрелка открытия меню
 let navCloseButton = document.getElementsByClassName('navCloseButton'); //Стрелка закрытия меню
-navCloseButton[0].style.display ='none';
+let statusInsuder = document.getElementById('status-insuder');          //Cтатус собственника/страхователя
+let menuInsuder = document.getElementsByClassName('f4');                //Меню данных страхователя
+let insuder = document.getElementsByClassName('menu-4')                 //Заголовок меню данных страхователя
 
-function navOpenFun(){
+navCloseButton[0].style.display = 'none';
+
+function navOpenFun() {
     navCloseButton[0].style.display = 'block';
     navOpenButton[0].style.display = 'none';
-    for(let i=0; i<=navItem.length; i++){
+    for (let i = 0; i <= navItem.length; i++) {
         navItem[i].style.display = 'block';
     }
 }
 
-function navCloseFun(){
-    navCloseButton[0].style.display ='none';
-    navOpenButton[0].style.display ='block';
-    for(let i=0; i<=navItem.length; i++){
+function navCloseFun() {
+    navCloseButton[0].style.display = 'none';
+    navOpenButton[0].style.display = 'block';
+    for (let i = 0; i <= navItem.length; i++) {
         navItem[i].style.display = 'none';
+    }
+}
+
+function checkedStatusInsuder() {
+    if (statusInsuder.checked === true) {
+        menuInsuder[0].style.display = 'none';
+        insuder[0].style.display = 'none';
+
+    }
+    else {
+        menuInsuder[0].style.display = 'flex';
+        insuder[0].style.display = 'block';
     }
 }
