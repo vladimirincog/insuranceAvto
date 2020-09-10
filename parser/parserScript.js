6aa078ce99413b30a19b7bf7650a6c75cfa7139f
Владимир <vladimirincog@users.noreply.github.com>
2020-09-11 01:09:45 +0300

    Update parserScript.js

--- parser/parserScript.js
+++ parser/parserScript.js
@@ -38,3 +38,30 @@ for (let i = 0; i <= li.length; i++) {
     }
 }
 console.log(JSON.stringify(arrModel));
+
+
+//Парсер https://www.soglasie.ru/avto/kalkulyator-eosago/#/auto
+/* 1. Селектор с маркой являющейся "кнопкой" для перехода к моделям авто
+<span data-v-2fd750e4="" class="car-list-item__text">AC</span> */
+
+
+const mark = document.querySelectorAll('span[class^="car-list-item"]');
+let arrMark = [];
+
+for(let i=0; i<mark.length; i++){
+    arrMark.push(mark[i].textContent);
+}
+
+/* 2. Кликаем по марке авто и получаем модели марки записываем их в строку соответствующей марки
+переходим на страницу назад и повторяем цикл для другой модели
+<span data-v-2fd750e4="" class="car-list-item__text">ACE</span> */
+/*<div data-v-2c05f226="" class="tabs__header-item _active">Марка</div> - кнопка для возврата назад*/
+const markButton = document.querySelectorAll('span[class^="tabs__header-item _active"]');
+
+for(let i=0; i<mark.length; i++){
+    mark[i].click();
+    let model = document.querySelectorAll('span[class^="car-list-item__text"]');
+    for(let j=0; j<model.length; j++){
+        arrMark[i][j+1]=model.textContent;
+    }
+}
