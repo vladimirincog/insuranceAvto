bb9f4fc7b3c380c1e24ce8d805574ad6a4254d68
Владимир <vladimirincog@users.noreply.github.com>
2020-10-02 20:58:31 +0300

    Create phone.php

---
+++ php/phone.php
@@ -0,0 +1,10 @@
+$callName = $_POST['callName'];
+    $callTel = $_POST['callTel'];
+    mail(
+        "vladimirincog@gmail.com",
+        "Заказ звонка с сайта",
+        "\nНомер: " . $callName .
+            "\nИмя: " . $callTel,
+        "Данные для звонка"
+    );
+    header("Location: https://insuranceavto.000webhostapp.com/");
