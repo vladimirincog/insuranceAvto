08d972130ef93fb58d3986947ddfc9829d014bd3
Владимир <vladimirincog@users.noreply.github.com>
2020-10-02 20:58:33 +0300

    Update mail.php

--- php/mail.php
+++ php/mail.php
@@ -181,16 +181,4 @@ mail(
 
         "Данные для страховки"
 );
-function callForm()
-{
-    $callName = $_POST['callName'];
-    $callTel = $_POST['callTel'];
-    mail(
-        "vladimirincog@gmail.com",
-        "Заказ звонка с сайта",
-        "\nНомер: " . $callName .
-            "\nИмя: " . $callTel,
-        "Данные для звонка"
-    );
-    header("Location: https://insuranceavto.000webhostapp.com/");
-}
+
