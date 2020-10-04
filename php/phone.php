68b59a6c281d4a878df24e5e19085accc2e94ca3
Владимир <vladimirincog@users.noreply.github.com>
2020-10-05 01:09:36 +0300

    Update phone.php

--- php/phone.php
+++ php/phone.php
@@ -1,11 +1,12 @@
 <?php
 $callName = $_POST['callName'];
 $callTel = $_POST['callTel'];
+
 mail(
     "9092345673@mail.ru",
     "Заказ звонка с сайта",
-    "\nНомер: " . $callName .
-        "\nИмя: " . $callTel,
+    "\nНомер: " . $callTel .
+        "\nИмя: " . $callName,
     "Данные для звонка"
 );
-header("Location: https://insuranceavto.000webhostapp.com/");
+echo '<script>location.replace("../index.html");</script>'; exit;
