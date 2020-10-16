a269df72643f0d8c910e4d2bda2d7cae182d1738
Владимир <vladimirincog@users.noreply.github.com>
2020-10-16 03:01:46 +0300

    Update phone-email.php

--- php/phone-email.php
+++ php/phone-email.php
@@ -5,7 +5,7 @@ $callName = $_POST['callName'];
 $callTel = $_POST['callTel'];
 
 mail(
-    "vladimirincog@gmail.com",
+    "9092345673@mail.ru",  //9092345673@mail.ru   vladimirincog@gmail.com
     "Заказ звонка с сайта",
     "\nНомер: " . $callTel .
         "\nИмя: " . $callName
